e5cf5269383818d1aab29e2d12f5ad54af8e02a2
Eazzi <eazzidanladi@gmail.com>
2025-06-26 23:42:56 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Placeholder for any future interactions (e.g., menu toggle)
+console.log("Site loaded");
+
+document.getElementById("card").onclick = function() {
+  window.location.href= "workpage.html"
+}
+document.getElementById("card2").onclick = function() {
+  window.location.href= "workpage.html"
+}
+document.getElementById("card3").onclick = function() {
+  window.location.href= "workpage.html"
+}
+
+
+document.getElementById("home").onclick = function() {
+  window.location.href= "home.html"
+}
+document.getElementById("design").onclick = function() {
+  window.location.href= "workpage.html"
+}
+document.getElementById("contact").onclick = function() {
+  window.location.href= "contact.html"
+}
